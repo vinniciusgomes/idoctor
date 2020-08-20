89df52affa4467aa5bbc99b917d9cd91b57d7188
Vinnicius Gomes <hello@vinniciusgomes.dev>
2020-08-19 22:05:04 -0300

    Finalizando boilerplate

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Container } from "./styles";
 
 export default function Dashboard() {
   return (
-    <div>
+    <Container>
       <h1>Dashboard</h1>
-    </div>
+    </Container>
   );
 }
