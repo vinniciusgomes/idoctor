46798349c28a420d62bfcb2bad40a0c6f9c635b8
Vinnicius Gomes <hello@vinniciusgomes.dev>
2020-08-21 20:39:04 -0300

    Melhorias no boilerplate

--- src/store/ducks/auth.ts
+++ src/store/ducks/auth.ts
@@ -2,7 +2,6 @@ import { createReducer, createActions } from "reduxsauce";
 import Immutable from "seamless-immutable";
 
 // Types & Actions Creators
-
 const { Types, Creators } = createActions({
   signInRequest: ["email", "password"],
   signInSuccess: ["token"],
@@ -12,14 +11,12 @@ export const AuthTypes = Types;
 export default Creators;
 
 // Initial State
-
 export const INITIAL_STATE = Immutable({
   signedIn: !!localStorage.getItem("@Omni:token"),
   token: localStorage.getItem("@Omni:token") || null,
 });
 
 // Reducers
-
 interface SuccessProps {
   token: string;
 }
@@ -27,7 +24,6 @@ export const success = (state: any, { token }: SuccessProps) =>
   state.merge({ signedIn: true, token });
 
 // Reducers to types
-
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.SIGN_IN_SUCCESS]: success,
 });
--- src/store/sagas/auth.ts
+++ src/store/sagas/auth.ts
@@ -1,9 +1,8 @@
 import { call, put } from "redux-saga/effects";
 import { actions as toastrActions } from "react-redux-toastr";
-
 import { push } from "connected-react-router";
-import api from "../../services/api";
 
+import api from "../../services/api";
 import AuthActions from "../ducks/auth";
 
 interface SignInData {
@@ -14,11 +13,8 @@ interface SignInData {
 export function* signIn({ email, password }: SignInData) {
   try {
     const response = yield call(api.post, "sessions", { email, password });
-
     localStorage.setItem("iDoctor:token", response.data.token);
-
     yield put(AuthActions.signInSuccess(response.data.token));
-
     yield put(push("/"));
   } catch (err) {
     yield put(
