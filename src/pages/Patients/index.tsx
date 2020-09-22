cb98e476cba1434bed524538d5a3a90533e98a6c
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2020-09-22 20:03:40 -0300

    Iniciando lista de pacientes

--- src/pages/Patients/index.tsx
+++ src/pages/Patients/index.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
 
-import { Container } from './styles';
+import { Container, ListContainer } from './styles';
 
-const Patients: React.FC = () => {
-  return (
-    <Navbar pageSelected="patients">
-      <Container>
-        <h1>Patients</h1>
-      </Container>
-    </Navbar>
-  );
-};
+const Patients: React.FC = () => (
+  <Navbar pageSelected="patients">
+    <Container>
+      <h1>Pacientes</h1>
+      <ListContainer>
+        <ul>
+          <li>Paciente 1</li>
+          <li>Paciente 1</li>
+          <li>Paciente 1</li>
+          <li>Paciente 1</li>
+          <li>Paciente 1</li>
+        </ul>
+
+        <footer>
+          <span>
+            Mostrando
+            {' '}
+            <b>10</b>
+            {' '}
+            de
+            {' '}
+            <b>350</b>
+            {' '}
+            pacientes
+          </span>
+
+          <div>
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+            <span>4</span>
+            <span>5</span>
+          </div>
+        </footer>
+      </ListContainer>
+    </Container>
+  </Navbar>
+);
 
 export default Patients;
--- src/pages/Patients/styles.ts
+++ src/pages/Patients/styles.ts
@@ -2,15 +2,77 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   flex: 1;
 
   h1 {
     font-size: 30px;
-    color: ${props => props.theme.colors.black};
+    color: ${(props) => props.theme.colors.black};
   }
+
   p {
     margin-top: 24px;
     font-size: 24px;
     line-height: 32px;
+  }
+`;
+
+export const ListContainer = styled.div`
+  width: 100%;
+  padding: 20px 40px;
+  margin-top: 40px;
+
+  background: #fff;
+  box-shadow: 0px 0px 10px #0000000d;
+  border-radius: 10px;
+
+  ul {
+    list-style: none;
+    li {
+      width: 100%;
+      border-bottom: 1px solid #eef0f2;
+      padding-bottom: 20px;
+
+      & + li {
+        margin-top: 20px;
+      }
+    }
+  }
+
+  footer {
+    margin: 30px 0 10px 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    div {
+      display: flex;
+      align-items: center;
+
+      > span {
+        display: block;
+
+        font-weight: 500;
+
+        &:first-child {
+          width: 25px;
+          height: 25px;
+
+          border-radius: 30px;
+          background: #7081fa;
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          color: #fff;
+        }
+
+        & + span {
+          margin-left: 20px;
+        }
+      }
+    }
   }
 `;
